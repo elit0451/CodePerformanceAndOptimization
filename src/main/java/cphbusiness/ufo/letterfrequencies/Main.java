df35663eac9a93b27da788d751a36240ebdd2902
Elitsa <elit0451@edu.eal.dk>
2019-11-10 14:25:09 +0100

    Optimized performance implementation

--- src/main/java/cphbusiness/ufo/letterfrequencies/Main.java
+++ src/main/java/cphbusiness/ufo/letterfrequencies/Main.java
@@ -1,9 +1,9 @@
 package cphbusiness.ufo.letterfrequencies;
 
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.IOException;
-import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
@@ -18,24 +18,29 @@ public class Main {
 
     public static void main(String[] args) throws FileNotFoundException, IOException {
         String fileName = "/letterfrequencies/src/main/resources/FoundationSeries.txt";
-        Reader reader = new FileReader(fileName);
-        Map<Integer, Long> freq = new HashMap<>();
-        tallyChars(reader, freq);
-        print_tally(freq);
+        char[] content = new String(Files.readAllBytes(Paths.get(fileName))).toCharArray();
+        Map<Character, Long> freq = new HashMap<>();
+        tallyChars(content, freq);
+        //print_tally(freq);
     }
 
-    private static void tallyChars(Reader reader, Map<Integer, Long> freq) throws IOException {
-        int b;
-        while ((b = reader.read()) != -1) {
-            try {
-                freq.put(b, freq.get(b) + 1);
-            } catch (NullPointerException np) {
-                freq.put(b, 1L);
-            };
-        }
+    private static void tallyChars(char[] content, Map<Character, Long> freq) throws IOException {
+        for(char c: content){
+            if(freq.containsKey(c))
+                freq.put(c, freq.get(c) + 1);
+            else
+                freq.put(c, 1L);
+            }
+//        while ((b = reader.read()) != -1) {
+//            try {
+//                freq.put(b, freq.get(b) + 1);
+//            } catch (NullPointerException np) {
+//                freq.put(b, 1L);
+//            };
+//        }
     }
 
-    private static void print_tally(Map<Integer, Long> freq) {
+    private static void print_tally(Map<Character, Long> freq) {
         int dist = 'a' - 'A';
         Map<Character, Long> upperAndlower = new LinkedHashMap();
         for (Character c = 'A'; c <= 'Z'; c++) {
